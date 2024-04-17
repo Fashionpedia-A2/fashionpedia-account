7983971916ffd0588aed01a14c27e41e9adbce6e
Nabilcodes <nabil.muhammad@ui.ac.id>
2024-04-17 16:26:03 +0700

    initial project setup

---
+++ src/main/java/id/my/fashionpediaapiaccount/FashionpediaapiaccountApplication.java
@@ -0,0 +1,13 @@
+package id.my.fashionpediaapiaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionpediaapiaccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionpediaapiaccountApplication.class, args);
+    }
+
+}
