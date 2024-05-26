2088a53fbc4d9da61d3866203110ead7fa9c0737
Fredo Melvern Tanzil <55704134+Frmeta@users.noreply.github.com>
2024-05-26 19:07:01 +0700

    Fix CORS origin (Update AuthController.java)

--- src/main/java/id/my/fashionpediaapiaccount/controller/AuthController.java
+++ src/main/java/id/my/fashionpediaapiaccount/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.logging.Logger;
 
+@CrossOrigin(origins = "*", allowedMethods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
